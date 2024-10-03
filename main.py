bdd9d83c2fd9a65a21ae446104a9bf3394d9f798
AbdullahAbaza <blackparrot6695@gmail.com>
2024-10-03 15:45:45 +0300

    feat: Add CRUD operations for posts
    - Implement GET /posts endpoint to retrieve all posts
    - Add GET /posts/{id} endpoint to fetch a specific post
    - Create POST /posts/create endpoint to add new posts
    - Introduce Post model with UUID, title, content, and optional fields
    - Set up temporary in-memory storage for posts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 
 
@@ -7,11 +7,15 @@ from typing import Optional
 from datetime import datetime
 import uuid
 
+
+
 app = FastAPI()
 
 
+
+
 class Post(BaseModel):
-    id : uuid.uuid4 = uuid.uuid4()
+    id : uuid.UUID = uuid.uuid4()
     title: str
     content: str
     published: bool = True
@@ -22,14 +26,22 @@ class Post(BaseModel):
 
 
 temp_posts = [
-    {"id": uuid.uuid4(), "title": "post1" , "content": "post1 content"},
-    {"id": uuid.uuid4(), "title": "post2" , "content": "post2 content"}
+    {"id": uuid.UUID("2ac9fe60-c9f9-41c0-a984-d09fd703c729"), "title": "post1", "content": "post1 content"},
+    {"id": uuid.UUID("75de8c59-739c-4577-b54f-2b9b9cfa90a8"), "title": "post2", "content": "post2 content"}
 ]
 
+
+def find_post(id: uuid.UUID):
+    for post in temp_posts:
+        if post["id"] == id:
+            return post
+    return None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
 
 @app.get("/posts")
 async def get_posts():
@@ -39,16 +51,31 @@ async def get_posts():
         }
 
 
+@app.get("/posts/{id}")
+async def get_post_by_id(id: uuid.UUID, response: Response):
+    post = find_post(id)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            "message": f"post with id: {id} was not found"
+        }
+    return {
+    "message": f"post found with id: {id}",
+    "data": post
+}
+
+
 
 @app.post("/posts/create")
 async def create_post(post: Post):
-    post_dict = post.dict()
-    print(post_dict)
+    post_dict = post.model_dump()
     temp_posts.append(post_dict)
     return {
-        "messasge": f"post created successfully with id: {post.id}",
-        "date": post
-        }
+        "message": f"post created successfully with id: {post.id}",
+        "data": post
+    }
+
+
 
 
 

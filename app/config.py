7a985993f7f4c8c79023445e48832fd7d5d75b74
AbdullahAbaza <abdullahabaza96@gmail.com>
2025-01-14 00:24:46 +0200

    refactor: enhance configuration settings and update token algorithm
    - Introduced a model configuration for environment variables in app/config.py.
    - Added a generated SECRET_KEY using secrets for improved security.
    - Set default values for DATABASE_PORT, DATABASE_PASSWORD, and DATABASE_NAME.
    - Updated ALGORITHM in app/oauth2.py to a fixed value "HS256" for consistency.

--- app/config.py
+++ app/config.py
@@ -1,19 +1,25 @@
-from pydantic_settings import BaseSettings
+import secrets
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        # Use top level .env file (one level above ./app/)
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    
     DATABASE_HOSTNAME: str
-    DATABASE_PORT: int
-    DATABASE_NAME: str
+    DATABASE_PORT: int = 5432
     DATABASE_USERNAME: str
-    DATABASE_PASSWORD: str
-    
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    
-    class Config:
-        env_file = ".env"
+    DATABASE_PASSWORD: str = ""
+    DATABASE_NAME: str = ""
     
 
 settings = Settings()
